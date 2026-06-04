17d6aad83de142e247674d307e5a061190e31bcc
sunny--mood <240340222@cauc.edu.cn>
2026-06-04 22:49:39 +0800

    敌人死后被销毁的问题

--- Source/playDemo/EnemyBase.cpp
+++ Source/playDemo/EnemyBase.cpp
@@ -1,6 +1,7 @@
 #include "EnemyBase.h"
 #include "Net/UnrealNetwork.h"
 #include "Kismet/GameplayStatics.h"
+#include "Engine/Engine.h"  // 添加这一行，用于屏幕打印
 
 AEnemyBase::AEnemyBase()
 {
@@ -25,21 +26,31 @@ void AEnemyBase::OnRep_CurrentHealth()
 {
 	// 当生命值更新时，这里可以触发蓝图中播放受伤动画等
 	// 蓝图中可以绑定这个事件
+	GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Yellow, TEXT("OnRep_CurrentHealth 被调用"));
 }
 
 void AEnemyBase::ApplyDamage(float DamageAmount)
 {
 	// 只在服务器上执行伤害逻辑
 	if (GetLocalRole() != ROLE_Authority)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TEXT("ApplyDamage: 不是服务器，跳过"));
 		return;
+	}
 
 	if (!IsAlive())
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TEXT("ApplyDamage: 敌人已死，跳过"));
 		return;
+	}
+
+	GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Green, FString::Printf(TEXT("敌人受到伤害: %.0f, 当前生命: %.0f"), DamageAmount, CurrentHealth));
 
 	CurrentHealth = FMath::Clamp(CurrentHealth - DamageAmount, 0.f, MaxHealth);
 
 	if (!IsAlive())
 	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TEXT("敌人死亡！"));
 		// 死亡：可以销毁，或者播放死亡动画后销毁
 		Destroy();
 	}
